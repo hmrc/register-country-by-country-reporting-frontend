03864bd16551662ce127b48086f2bf4366774667
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-06-06 09:40:41 +0100

    DAC6-1741: Add BusinessName page

--- app/forms/mappings/Formatters.scala
+++ app/forms/mappings/Formatters.scala
@@ -37,6 +37,49 @@ trait Formatters {
       Map(key -> value)
   }
 
+  protected def textMaxLengthFormatter(requiredKey: String, lengthKey: String, maxLength: Int): Formatter[String] = new Formatter[String] {
+    private val dataFormatter: Formatter[String] = stringTrimFormatter(requiredKey)
+
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] =
+      dataFormatter
+        .bind(key, data)
+        .right
+        .flatMap {
+          case str if str.length > maxLength => Left(Seq(FormError(key, lengthKey)))
+          case str                           => Right(str)
+        }
+
+    override def unbind(key: String, value: String): Map[String, String] =
+      Map(key -> value)
+  }
+
+  private[mappings] def stringTrimFormatter(errorKey: String, msgArg: String = ""): Formatter[String] = new Formatter[String] {
+
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] =
+      data.get(key) match {
+        case None =>
+          msgArg.isEmpty match {
+            case true  => Left(Seq(FormError(key, errorKey)))
+            case false => Left(Seq(FormError(key, errorKey, Seq(msgArg))))
+          }
+        case Some(s) =>
+          s.trim match {
+            case "" =>
+              msgArg.isEmpty match {
+                case true  => Left(Seq(FormError(key, errorKey)))
+                case false => Left(Seq(FormError(key, errorKey, Seq(msgArg))))
+              }
+            case s1 => Right(removeNonBreakingSpaces(s1))
+          }
+      }
+
+    override def unbind(key: String, value: String): Map[String, String] =
+      Map(key -> value)
+  }
+
+    private def removeNonBreakingSpaces(str: String) =
+      str.replaceAll("\u00A0", " ")
+
   private[mappings] def booleanFormatter(requiredKey: String, invalidKey: String, args: Seq[String] = Seq.empty): Formatter[Boolean] =
     new Formatter[Boolean] {
 
---
+++ test/forms/BusinessNameFormProviderSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import models.BusinessType.LimitedCompany
+import play.api.data.FormError
+
+class BusinessNameFormProviderSpec extends StringFieldBehaviours {
+
+  val requiredKey = s"businessName.error.required.$LimitedCompany"
+  val lengthKey = s"businessName.error.length.$LimitedCompany"
+  val maxLength = 105
+
+  val form = new BusinessNameFormProvider()(LimitedCompany)
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithMaxLengthAlpha(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
