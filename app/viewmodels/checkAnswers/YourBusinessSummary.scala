8fc779e27cedca0ff48602d6373933c61c3444b9
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-06-23 10:12:26 +0100

    DAC6-1793: Add id

--- app/viewmodels/checkAnswers/YourBusinessSummary.scala
+++ app/viewmodels/checkAnswers/YourBusinessSummary.scala
@@ -63,7 +63,7 @@ object YourBusinessSummary {
               value   = ValueViewModel(HtmlContent(value)),
               actions = Seq(
                 ActionItemViewModel("site.change", routes.DoYouHaveUTRController.onPageLoad(CheckMode).url)
-                  .withVisuallyHiddenText(messages("businessWithIDName.change.hidden"))
+                  .withVisuallyHiddenText(messages("businessWithIDName.change.hidden")).withAttribute(("id","your-business-details"))
               )
             ))
           case _ => None
