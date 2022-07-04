6bf9fdc59486a9d92fd4f9fa2cc1a7ef0e163e41
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-07-04 13:40:10 +0100

    DAC6-1822: Fix for session expiring before timeout redirect

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -17,10 +17,8 @@
 package controllers.auth
 
 import config.FrontendAppConfig
-import controllers.actions.IdentifierAction
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.Inject
@@ -29,29 +27,17 @@ import scala.concurrent.ExecutionContext
 class AuthController @Inject() (
   val controllerComponents: MessagesControllerComponents,
   config: FrontendAppConfig,
-  sessionRepository: SessionRepository,
-  identify: IdentifierAction
   )(implicit ec: ExecutionContext)
   extends FrontendBaseController
     with I18nSupport {
 
-  def signOut(): Action[AnyContent] = identify.async {
+  def signOut(): Action[AnyContent] = Action {
     implicit request =>
-      sessionRepository
-        .clear(request.userId)
-        .map {
-          _ =>
-            Redirect(config.signOutUrl).withSession()
-        }
+      Redirect(config.signOutUrl).withNewSession
   }
 
-  def signOutNoSurvey(): Action[AnyContent] = identify.async {
+  def signOutNoSurvey(): Action[AnyContent] = Action {
     implicit request =>
-      sessionRepository
-        .clear(request.userId)
-        .map {
-          _ =>
-            Redirect(routes.SignedOutController.onPageLoad.url).withSession()
-        }
+      Redirect(routes.SignedOutController.onPageLoad).withNewSession
   }
 }
