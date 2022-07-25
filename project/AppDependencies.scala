7c992c44d0cb81936c9965e513cdd6ebf8a90a2d
“mahipal-padala” <22635253+mahipal-padala@users.noreply.github.com>
2022-07-25 12:53:19 +0100

    DAC6-1910: Updated hmrc-mongo-play-28 to 0.68.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.9.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.24.0",
     "uk.gov.hmrc"       %% "play-language"                  % "5.1.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.59.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0"
   )
 
   val test = Seq(
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.mockito"             %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"          %% "scalacheck"              % "1.15.4",
     "wolfendale"             %% "scalacheck-gen-regexp" % "0.1.2",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-27" % "0.59.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "0.68.0",
     "com.github.tomakehurst"  % "wiremock-jre8"            % "2.26.0",
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
