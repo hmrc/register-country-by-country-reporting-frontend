cc6ee08e9e9a23e89f318068ea51a4532077c859
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2023-01-17 16:28:39 +0000

    DAC6-1834: update library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.2.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.11.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.12.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.74.0"
   )
 
