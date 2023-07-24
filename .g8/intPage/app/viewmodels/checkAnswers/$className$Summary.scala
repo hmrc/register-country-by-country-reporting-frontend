42a2c1c274351ef0871bf395ffe06f5de4124203
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2023-07-24 09:59:31 +0100

    DAC6-scaffoldFix: Fix scaffold templates

--- .g8/intPage/app/viewmodels/checkAnswers/$className$Summary.scala
+++ .g8/intPage/app/viewmodels/checkAnswers/$className$Summary.scala
@@ -4,6 +4,7 @@ import controllers.routes
 import models.{CheckMode, UserAnswers}
 import pages.$className$Page
 import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
@@ -22,11 +23,12 @@ object $className$Summary  {
               content = HtmlContent(
                 s"""
                    |<span aria-hidden="true">${messages("site.change")}</span>
-                   |<span class="govuk-visually-hidden">${messages("$className;format="decap"$.change.hidden")}</span>
+                   |<span class="govuk-visually-hidden">\${messages("$className;format="decap"$.change.hidden")}</span>
                    |""".stripMargin
               ),
               href = routes.$className$Controller.onPageLoad(CheckMode).url
             )
-        )
+          )
+      )
     }
 }
