f46d0c0e2f50d0acbd1d6807b9949718d66a0e35
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-04-16 08:43:13 +0100

    DAC6-3076 | Amended displayed country names and removed unused imports

--- app/controllers/DifferentBusinessController.scala
+++ app/controllers/DifferentBusinessController.scala
@@ -18,7 +18,6 @@ package controllers
 
 import config.FrontendAppConfig
 import controllers.actions._
-import models.matching.RegistrationInfo
 import pages.RegistrationInfoPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
