ec54765bb96f56baece2ae897e60f79d3e48c665
Carl Thompson <carl.thompson@opencastsoftware.com>
2024-08-07 14:36:48 +0100

    dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,9 +19,9 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"      %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0",
-    "org.mockito"            %% "mockito-scala"           % "1.17.31",
+    "org.mockito"            %% "mockito-scala"           % "1.17.37",
     "org.pegdown"             % "pegdown"                 % "1.6.0",
-    "org.jsoup"               % "jsoup"                   % "1.17.2",
+    "org.jsoup"               % "jsoup"                   % "1.18.1",
     "wolfendale"             %% "scalacheck-gen-regexp"   % "0.1.2",
   ).map(_ % Test)
 
