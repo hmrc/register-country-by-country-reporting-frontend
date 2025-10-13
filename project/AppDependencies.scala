6cf1c641f9e8215f446eaa79072aa44e9e406629
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2025-10-13 15:17:20 +0100

    DAC6-3344 | updated dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.18.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoVersion = "2.9.0"
 
   val compile = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.8.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.17.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion
