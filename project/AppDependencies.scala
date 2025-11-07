efb8fa54baff16c596b506eaf338dfe65c2af47d
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2025-11-07 12:44:02 +0000

    DAC6-3965 | update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.4.0"
   private val hmrcMongoVersion = "2.10.0"
 
 
   val compile: Seq[ModuleID] = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.17.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.20.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
