d109aa874e2410f1bae605a43f970e328d1a376c
Carl Thompson <carl.thompson@opencastsoftware.com>
2025-12-16 11:59:05 +0000

    DAC6-4069 | merge main and address pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.22.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion
   )
