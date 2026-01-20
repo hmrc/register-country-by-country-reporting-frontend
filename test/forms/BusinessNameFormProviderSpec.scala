d64e9ab2be9d30a0df8dcf1b85d4917793f85e0a
Carl Thompson <77161245+C-Thomp@users.noreply.github.com>
2026-01-20 15:31:04 +0000

    Dac6 4040 | update error message (#191)
    * DAC6-4040 | add new regex for each business names, messages and update spec
    
    * DAC6-4056 | update invalid massage
    
    * DAC6-4040 | add missing partnership key for invalid
    
    * DAC6-4040 | add specs for error messaging
    
    * DAC6-4040 | adjust message content
    
    * DAC6-4040 | update spec and adjust error message content

--- test/forms/BusinessNameFormProviderSpec.scala
+++ test/forms/BusinessNameFormProviderSpec.scala
@@ -15,56 +15,99 @@
  */
 
 package forms
-
+import base.SpecBase
 import forms.behaviours.StringFieldBehaviours
-import models.BusinessType.LimitedCompany
+import models.BusinessType.*
+import org.scalatest.prop.TableDrivenPropertyChecks
 import play.api.data.FormError
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.test.FakeRequest
 import wolfendale.scalacheck.regexp.RegexpGen
 
-class BusinessNameFormProviderSpec extends StringFieldBehaviours {
-
-  val requiredKey = s"businessName.error.required.$LimitedCompany"
-  val lengthKey   = s"businessName.error.length.$LimitedCompany"
-  val invalidKey  = s"businessName.error.invalid.$LimitedCompany"
-  val maxLength   = 105
+class BusinessNameFormProviderSpec extends SpecBase with StringFieldBehaviours with TableDrivenPropertyChecks {
+  implicit val messages: Messages = app.injector.instanceOf[MessagesApi].preferred(FakeRequest())
+  private val maxLength           = 105
 
   val form = new BusinessNameFormProvider()(LimitedCompany)
+  val businessTypeWithMessages =
+    Table(
+      ("businessType", "requiredMsg", "lengthMsg", "invalidMsg"),
+      (
+        LimitedCompany,
+        "Enter the registered name of your business",
+        "Business name must be 105 characters or less",
+        "Business name must only include letters a to z, numbers 0 to 9 and special characters such as hyphens, spaces and apostrophes"
+      ),
+      (
+        Partnership,
+        "Enter the partnership name",
+        "Partnership name must be 105 characters or less",
+        "Partnership name must only include letters a to z, numbers 0 to 9 and special characters such as hyphens, spaces and apostrophes"
+      ),
+      (
+        LimitedPartnership,
+        "Enter the registered name of your business",
+        "Business name must be 105 characters or less",
+        "Business name must only include letters a to z, numbers 0 to 9 and special characters such as hyphens, spaces and apostrophes"
+      ),
+      (
+        UnincorporatedAssociation,
+        "Enter the name of your organisation",
+        "Organisation name must be 105 characters of less",
+        "Organisation name must only include letters a to z, numbers 0 to 9 and special characters such as hyphens, spaces and apostrophes"
+      )
+    )
 
-  ".value" - {
+  forAll(businessTypeWithMessages) { (businessType, requiredMsg, lengthMsg, invalidMsg) =>
+    s".value for $businessType" - {
 
-    "normalises curly apostrophes to straight ones" in {
-      val result = form.bind(Map("value" -> "“‘apostrophes’”"))
-      result.errors mustBe empty
-      result.value.value mustBe "\"'apostrophes'\""
+      val requiredKey = s"businessName.error.required.$businessType"
+      val lengthKey   = s"businessName.error.length.$businessType"
+      val invalidKey  = s"businessName.error.invalid.$businessType"
+
+      val form      = new BusinessNameFormProvider()(businessType)
+      val fieldName = "value"
+
+      "normalises curly apostrophes to straight ones" in {
+        val result = form.bind(Map("value" -> "“‘apostrophes’”"))
+        result.errors mustBe empty
+        result.value.value mustBe "\"'apostrophes'\""
+      }
+
+      behave like fieldThatBindsValidDataWithoutInvalidError(
+        form,
+        fieldName,
+        RegexpGen.from(businessNameRegex),
+        invalidKey
+      )
+
+      behave like fieldWithInvalidData(
+        form,
+        fieldName,
+        "some emoji 🚀",
+        FormError(fieldName, invalidKey)
+      )
+
+      behave like fieldWithMaxLengthAlpha(
+        form,
+        fieldName,
+        maxLength = maxLength,
+        lengthError = FormError(fieldName, lengthKey)
+      )
+
+      behave like mandatoryField(
+        form,
+        fieldName,
+        requiredError = FormError(fieldName, requiredKey)
+      )
+
+      "have correct error message content" in {
+
+        messages(requiredKey) mustBe requiredMsg
+        messages(lengthKey) mustBe lengthMsg
+        messages(invalidKey) mustBe invalidMsg
+      }
     }
-
-    val fieldName = "value"
-
-    behave like fieldThatBindsValidDataWithoutInvalidError(
-      form,
-      fieldName,
-      RegexpGen.from(businessNameRegex),
-      invalidKey
-    )
-
-    behave like fieldWithInvalidData(
-      form,
-      fieldName,
-      "some emoji 🚀",
-      FormError(fieldName, invalidKey)
-    )
-
-    behave like fieldWithMaxLengthAlpha(
-      form,
-      fieldName,
-      maxLength = maxLength,
-      lengthError = FormError(fieldName, lengthKey)
-    )
-
-    behave like mandatoryField(
-      form,
-      fieldName,
-      requiredError = FormError(fieldName, requiredKey)
-    )
   }
+
 }
