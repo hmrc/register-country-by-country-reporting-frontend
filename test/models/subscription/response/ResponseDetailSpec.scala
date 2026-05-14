626cf67ccee406e9121af7c9956c3fb2ae508fd1
witchelljack <36855484+WitchellJack@users.noreply.github.com>
2026-05-14 11:33:00 +0100

    DAC6-4214 - added in test spec for responseDetail model

---
+++ test/models/subscription/response/ResponseDetailSpec.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.subscription.response
+
+import base.SpecBase
+import play.api.libs.json.Json
+
+class ResponseDetailSpec extends SpecBase {
+
+  "ResponseDetail" - {
+
+    "must serialise and deserialise with a primary contact only" in {
+      val responseDetail = ResponseDetail(
+        subscriptionID = "subscription-id",
+        tradingName = Some("Trading Name"),
+        isGBUser = true,
+        primaryContact = ContactInformation(
+          organisationDetails = OrganisationDetails("Primary Organisation"),
+          email = "primary@test.com",
+          phone = Some("01234567890"),
+          mobile = Some("07123456789")
+        ),
+        secondaryContact = None
+      )
+
+      val json = Json.obj(
+        "subscriptionID" -> "subscription-id",
+        "tradingName"    -> "Trading Name",
+        "isGBUser"       -> true,
+        "primaryContact" -> Json.obj(
+          "organisationDetails" -> Json.obj(
+            "organisationName" -> "Primary Organisation"
+          ),
+          "email"  -> "primary@test.com",
+          "phone"  -> "01234567890",
+          "mobile" -> "07123456789"
+        )
+      )
+
+      Json.toJson(responseDetail) mustBe json
+      json.as[ResponseDetail] mustBe responseDetail
+    }
+
+    "must serialise and deserialise with a secondary contact" in {
+      val responseDetail = ResponseDetail(
+        subscriptionID = "subscription-id",
+        tradingName = None,
+        isGBUser = false,
+        primaryContact = ContactInformation(
+          organisationDetails = OrganisationDetails("Primary Organisation"),
+          email = "primary@test.com",
+          phone = None,
+          mobile = None
+        ),
+        secondaryContact = Some(
+          ContactInformation(
+            organisationDetails = OrganisationDetails("Secondary Organisation"),
+            email = "secondary@test.com",
+            phone = Some("09876543210"),
+            mobile = None
+          )
+        )
+      )
+
+      val json = Json.obj(
+        "subscriptionID" -> "subscription-id",
+        "isGBUser"       -> false,
+        "primaryContact" -> Json.obj(
+          "organisationDetails" -> Json.obj(
+            "organisationName" -> "Primary Organisation"
+          ),
+          "email" -> "primary@test.com"
+        ),
+        "secondaryContact" -> Json.obj(
+          "organisationDetails" -> Json.obj(
+            "organisationName" -> "Secondary Organisation"
+          ),
+          "email" -> "secondary@test.com",
+          "phone" -> "09876543210"
+        )
+      )
+
+      Json.toJson(responseDetail) mustBe json
+      json.as[ResponseDetail] mustBe responseDetail
+    }
+  }
+}
