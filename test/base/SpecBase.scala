19ecf14d5e52720fc9d36578b7fd00c2482b2653
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-05-15 11:35:09 +0100

    DAC6-4207 | removed unused imports

--- test/base/SpecBase.scala
+++ test/base/SpecBase.scala
@@ -35,9 +35,7 @@ import play.api.mvc.Call
 import play.api.test.FakeRequest
 import queries.Settable
 import repositories.SessionRepository
-import uk.gov.hmrc.auth.core.Enrolment
 import uk.gov.hmrc.http.HeaderCarrier
-import scala.concurrent.ExecutionContext.Implicits.global
 
 import java.time.{Clock, Instant, ZoneId}
 
